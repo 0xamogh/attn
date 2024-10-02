6b81c7ee24757d0e895034451371b7ca67d63055
Alok Potadar <Alok.Potadar@thorogood.com>
2024-10-02 12:05:22 +0530

    Added elements in manage-requests

---
+++ app/components/PendingPage.tsx
@@ -0,0 +1,11 @@
+const PendingPage = (): JSX.Element => {
+    return (
+      <div>
+        <h2>Pending Requests</h2>
+        <p>This is the Pending page content.</p>
+      </div>
+    );
+  };
+  
+  export default PendingPage;
+  
---
+++ app/components/SentPage.tsx
@@ -0,0 +1,11 @@
+const SentPage = (): JSX.Element => {
+    return (
+      <div>
+        <h2>Sent Requests</h2>
+        <p>This is the Sent page content.</p>
+      </div>
+    );
+  };
+  
+  export default SentPage;
+  
