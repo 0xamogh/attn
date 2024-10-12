2e1776c48c102ca473f192e5e0dd8f6f9f49231f
Amogh Jahagirdar <amoghjahagirdargsf@gmail.com>
2024-10-12 18:02:12 +0530

    done

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { useAccount, useConnect } from 'wagmi';
 import auth, { provider } from '../lib/firebase/auth';
 import { TwitterAuthProvider, signInWithPopup } from 'firebase/auth';
 import db from '../lib/firebase/firestore';
-import { doc, setDoc, updateDoc } from "firebase/firestore"; // Firestore imports
+import { doc, getDoc, setDoc, updateDoc } from "firebase/firestore"; // Firestore imports
 import { httpsCallable } from 'firebase/functions';
 import functions  from '../lib/firebase/functions';
 import { callFetchTwitterFollowers } from './helpers/functions';
@@ -17,101 +17,151 @@ import { useAuth } from './context/authContext';
 export default function Home() {
   const { connectors, connect } = useConnect();
   const account = useAccount();
-  
+
   // State to check if the user is signed in with Twitter
   const [isSignedIn, setIsSignedIn] = useState(false);
   const [userId, setUserId] = useState<string>(); // Store the Firebase user id
-  const [twitterUsername, setTwitterUsername] = useState<string>()
+  const [twitterUsername, setTwitterUsername] = useState<string>();
   const [toastVisible, setToastVisible] = useState(false);
   const { user, loading } = useAuth();
+  const [userData, setUserData] = useState<any>(); // State to store user data
+  const [rangeValue, setRangeValue] = useState(0.1); // Default value set to 50 (you can set this to any number within the range)
+  const [telegramId, setTelegramId] = useState<string>()
+  const [toastUpdateProfile, setToastUpdateProfile] =useState(false)
 
-   const handleCopy = () => {
-     const profileUrl = window.location.origin + "/profile/" + twitterUsername; // Your specific URL to be copied
-     navigator.clipboard.writeText(profileUrl).then(() => {
-       setToastVisible(true); // Show the toast
-       setTimeout(() => setToastVisible(false), 3000); // Hide the toast after 3 seconds
-     });
-   };
+  const handleCopy = () => {
+    const profileUrl = window.location.origin + "/profile/" + twitterUsername; // Your specific URL to be copied
+    navigator.clipboard.writeText(profileUrl).then(() => {
+      setToastVisible(true); // Show the toast
+      setTimeout(() => setToastVisible(false), 3000); // Hide the toast after 3 seconds
+    });
+  };
+
+    // const handleUpdate = () => {
+    //   const profileUrl = window.location.origin + "/profile/" + twitterUsername; // Your specific URL to be copied
+    //   navigator.clipboard.writeText(profileUrl).then(() => {
+    //     setToastVisible(true); // Show the toast
+    //     setTimeout(() => setToastVisible(false), 3000); // Hide the toast after 3 seconds
+    //   });
+    // };
 
   // Function to create wallet
   const createWallet = useCallback(() => {
     const coinbaseWalletConnector = connectors.find(
-      (connector) => connector.id === 'coinbaseWalletSDK'
+      (connector) => connector.id === "coinbaseWalletSDK"
     );
     if (coinbaseWalletConnector) {
-      connect({ connector: coinbaseWalletConnector })
+      connect({ connector: coinbaseWalletConnector });
     }
   }, [connectors, connect, account.address, userId]);
 
   // Function to handle Twitter sign-in and Firestore user creation
   const onSignInWithTwitter = () => {
-    signInWithPopup(auth, provider)
-      .then(async (result) => {
-        console.log("^_^ ~ file: page.tsx:39 ~ .then ~ result:", result);
+    signInWithPopup(auth, provider).then(async (result) => {
+      console.log("^_^ ~ file: page.tsx:39 ~ .then ~ result:", result);
 
-        // This gives you the Twitter OAuth 1.0 Access Token and Secret.
-        const credential = TwitterAuthProvider.credentialFromResult(result);
-        const token = credential?.accessToken;
-        const secret = credential?.secret;
+      // The signed-in user info.
+      const user = result.user;
+      //@ts-ignore
+      console.log("^_^ ~ file: page.tsx:48 ~ .then ~ user:", user.screenName);
 
-        // The signed-in user info.
-        const user = result.user;
-        //@ts-ignore
-        console.log("^_^ ~ file: page.tsx:48 ~ .then ~ user:", user.screenName);
+      //@ts-ignore
+      const twitterUsername = user.reloadUserInfo.screenName;
 
-        //@ts-ignore
-        const twitterUsername = user.reloadUserInfo.screenName;
+      setUserId(user.uid);
+      setIsSignedIn(true);
+      setTwitterUsername(twitterUsername);
 
-        setUserId(user.uid);
-        setIsSignedIn(true);
-        setTwitterUsername(twitterUsername);
+      // Create a Firestore document for the user
+      const userDocRef = doc(db, "users", user.uid);
+      setDoc(
+        userDocRef,
+        {
+          name: user.displayName,
+          email: user.email,
+          twitterId: user.providerData[0].uid,
+          photoUrl: user.photoURL,
+          twitterUsername: twitterUsername,
+        },
+        {
+          merge: true,
+        }
+      );
+    });
+  };
 
-        // Create a Firestore document for the user
-        const userDocRef = doc(db, "users", user.uid);
-        setDoc(
-          userDocRef,
-          {
-            name: user.displayName,
-            email: user.email,
-            twitterId: user.providerData[0].uid,
-            photoUrl: user.photoURL,
-            twitterUsername: twitterUsername,
-          },
-          {
-            merge: true,
-          }
-        );
-      })
-
-
-    }
-
-    useEffect(()=> {
-      if(user){
-        setIsSignedIn(true)
+  useEffect(() => {
+    const fetchUserFromDB = async (uid: string) => {
+      try {
+        // Query the user document from Firestore using the user's UID
+        const userDocRef = doc(db, "users", uid);
+        const userDoc = await getDoc(userDocRef);
+        if (userDoc.exists()) {
+          // Store the fetched user data in state
+          setUserData(userDoc.data());
+        } else {
+          console.error("No such document!");
+        }
+      } catch (error) {
+        console.error("Error fetching user data: ", error);
       }
-    },[account, user])
-    // UseEffect to check and print once the account address and userId are available
+    };
+
+    if (user) {
+      setIsSignedIn(true); // Update sign-in state
+
+      // Fetch the full user document from Firestore
+      fetchUserFromDB(user.uid);
+    }
+  }, [account, user]);
+
+  // UseEffect to check and print once the account address and userId are available
   useEffect(() => {
     if (account.address && userId) {
       // Print the userId and account.address when they are available
-      console.log('User ID:', userId);
-      console.log('Wallet Address:', account.address);
+      console.log("User ID:", userId);
+      console.log("Wallet Address:", account.address);
 
       // Update Firestore with the wallet address (this part is just for printing)
       const userDocRef = doc(db, "users", userId);
       updateDoc(userDocRef, {
-        walletAddress: account.address
+        walletAddress: account.address,
       })
-      .then(() => {
-        console.log("Wallet address updated in Firestore");
-      })
-      .catch((error) => {
-        console.error("Error updating wallet address:", error);
-      });
+        .then(() => {
+          console.log("Wallet address updated in Firestore");
+        })
+        .catch((error) => {
+          console.error("Error updating wallet address:", error);
+        });
     }
   }, [account.address, userId]); // Trigger useEffect when account.address or userId changes
 
+    const handleRangeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setRangeValue(event.target.valueAsNumber); // Update state with the value of the range input
+    };
+
+     const handleTelegramIdChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+       setTelegramId(event.target.value); // Update state with the value of the range input
+     };
+     const handleUpdateProfile = async () => {
+       if (user && user.uid) {
+         try {
+           // Get a reference to the user's document
+           const userDocRef = doc(db, "users", user.uid);
+
+           // Update the user's profile with the new rangeValue and telegramId
+           await updateDoc(userDocRef, {
+             price: rangeValue,
+             telegramID: telegramId,
+           });
+  setToastUpdateProfile(true); // Show the toast
+  setTimeout(() => setToastUpdateProfile(false), 3000); // Hide the toast after 3 seconds
+           console.log("Profile updated successfully");
+         } catch (error) {
+           console.error("Error updating profile: ", error);
+         }
+       }
+     };
 
   return (
     <>
@@ -154,6 +204,28 @@ export default function Home() {
                   Your connected account is {account.address}
                 </h2>
 
+                <h2 className={"font-bold text-black " + open.className}>
+                  Your base price is <br />
+                  {rangeValue} ETH
+                </h2>
+                <input
+                  type="range"
+                  min={"0.01"}
+                  max="1"
+                  step="0.01"
+                  value={rangeValue}
+                  onChange={handleRangeChange}
+                  className="range range-primary range-xs"
+                />
+                <h2 className={"font-bold text-black " + open.className}>
+                  Your Telegram Id is
+                </h2>
+                <input
+                  type="text"
+                  placeholder="Type here"
+                  className="input m-2 w-full max-w-xs text-black"
+                  onChange={handleTelegramIdChange}
+                />
                 {/* Render "Go to Manage Requests" button after the user is signed in and wallet is connected */}
                 <Link href="/manage-requests">
                   <button
@@ -162,9 +234,18 @@ export default function Home() {
                       open.className
                     }
                   >
-                    Go to Manage Requests
+                    Manage Requests
                   </button>
                 </Link>
+                <button
+                  className={
+                    "m-6 btn btn-md rounded-full btn-neutral shadow-xl text-white " +
+                    open.className
+                  }
+                  onClick={handleUpdateProfile}
+                >
+                  Update Profile
+                </button>
                 <button
                   className={
                     "m-6 btn btn-md rounded-full btn-primary shadow-xl text-white " +
@@ -183,6 +264,13 @@ export default function Home() {
         <div className="toast">
           <div className="alert text-white alert-info">
             <span>Link copied to clipboard!</span>
+          </div>
+        </div>
+      )}
+      {toastUpdateProfile && (
+        <div className="toast">
+          <div className="alert text-white alert-info">
+            <span>Profile updated!</span>
           </div>
         </div>
       )}
